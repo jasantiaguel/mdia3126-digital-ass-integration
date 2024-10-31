a9efd265c48f24ebbabd3048cf833862c3ff31e0
Jon Santiaguel <jsantiaguel2@my.bcit.ca>
2024-10-31 12:06:12 -0700

    api fetch and display with button

--- midterm-review/src/app/page.js
+++ midterm-review/src/app/page.js
@@ -1,95 +1,81 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  // TODO
+  //
+  // button that fetches and displays 5 results
+  //  - handle and format content
+  //  - display (empty, loading, fulfilled state)
+  //  - includes 3 separate elements, including media
+  //    - media, title, date
+  //  - styling
+  //    - breakpoints (mobile-first)
+  //  - clear data
+
+  const [loading, setLoading] = useState(false);
+  const [imageData, setImageData] = useState(null);
+
+  async function fetchImages() {
+    const API_URL = "https://picsum.photos/v2/list?limit=5";
+    setLoading(true);
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    setImageData(data);
+    setLoading(false);
+  }
+
+  const Header = () => {
+    return (
+      <section>
+        <button className={styles.button} onClick={fetchImages}>
+          Fetch 📸
+        </button>
+      </section>
+    );
+  };
+
+  const ImageListContainer = () => {
+    if (loading) {
+      return <section>Loading...</section>;
+    }
+    if (imageData) {
+      const imageListItems = [];
+      imageData.forEach((image, i) => {
+        // author: "Alejandro Escamilla"
+        // download_url: "https://picsum.photos/id/0/5000/3333"
+        // height: 3333
+        // id: "0"
+        // url: "https://unsplash.com/photos/yC-Yzbqy7PY"
+        // width: 5000
+        imageListItems.push(
+          <article key={image.id}>
+            <img src={image.download_url} width="500"/>
+            <p>Author: {image.author}</p>
+            <a href={image.url}>View this image on Unsplash</a>
+            <hr />
+          </article>
+        );
+      });
+
+      return <section style={{ maxwidth: '50%' }}>{imageListItems}</section>;
+    }
+
+    return <section>no images fetched</section>;
+  };
+
+  const DataDisplay = () => {
+    return <section>displaying data</section>;
+  };
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <Header />
+      <ImageListContainer />
+      <DataDisplay />
     </div>
   );
 }
